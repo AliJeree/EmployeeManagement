6d2fd043bdcff66229094dc20392ba37f8f67191
Jeremy Aliwa <aliwajeremy1@gmail.com>
2024-06-07 11:53:59 +0300

    Third Commit

--- Employee_Management/emplmgtsystem-backend/src/main/java/com/jeremy/emplmgtsystembackend/exception/EmailAlreadyExistsException.java
+++
@@ -1,9 +0,0 @@
-package com.jeremy.emplmgtsystembackend.exception;
-
-public class EmailAlreadyExistsException extends RuntimeException {
-	
-	public EmailAlreadyExistsException(String message) {
-        super(message);
-
-}
-}
--- Employee_Management/emplmgtsystem-backend/src/main/java/com/jeremy/emplmgtsystembackend/repository/PaymentRepo.java
+++
@@ -1,8 +0,0 @@
-package com.jeremy.emplmgtsystembackend.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.jeremy.emplmgtsystembackend.entity.Payment;
-
-public interface PaymentRepo extends JpaRepository<Payment, Long> {
-}
